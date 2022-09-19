14b943b3905a16fbe6f3c41e5e507612370c0ec9
Angelwized <38271424+Angelwized@users.noreply.github.com>
2022-09-19 17:29:39 +0800

    Update mitm.js

--- mitm.js
+++ mitm.js
@@ -1,2 +1,3 @@
 # 修复Scriptable不可访问的问题
 hostname=boxjs.net,boxjs.com
+https?:\/\/boxjs\.(com|net) url script-analyze-echo-response https://raw.githubusercontent.com/chavyleung/scripts/master/box/chavy.boxjs.js
